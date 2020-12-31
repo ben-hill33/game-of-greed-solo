4e33abafc476e6927c6b315789fb76f2b96a240a
BEN <benjamin.hill28@gmail.com>
2020-12-30 16:19:06 -0800

    commiting changes

---
+++ game_of_greed/game_logic.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+class GameLogic:
+  def __init__(self, dice_roll, score=0, roll_round=0):
+    self.dice_roll = dice_roll
+    self.score = score
+    self.roll_round = roll_round
+
+  @staticmethod
+  def calculate_score(self, dice):
+    pass
---
+++ tests/test_game_of_greed.py
@@ -0,0 +1,6 @@
+from game_of_greed import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
+
